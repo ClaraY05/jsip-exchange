7c5c0beda04482bf1b19b99033afb29ac2fae601
Clara Yee <cy2005024@gmail.com>
2026-07-08 17:17:05 +0000

    feat: 0b implemented sequential data structures

--- performance/src/jsip_exchange_perf_lib.ml
+++ performance/src/jsip_exchange_perf_lib.ml
@@ -3,7 +3,7 @@ open Core_bench
 
 (* Sizes are kept modest: [Silly_store] is O(n) per operation, so [build] is
    ~O(n^2). *)
-let sizes = [ 10; 100; 1000 ]
+let sizes = [ 10; 30; 100; 300; 1000 ]
 let present_key n = n / 2
 let absent_key = -1
 
@@ -43,9 +43,21 @@ let bench_silly =
    side-effecting [List.init], whose evaluation order isn't guaranteed
    left-to-right. *)
 let seq_tests ~name ~create ~set ~get =
-  (* "TODO: benchmark for part 4, 0b" *)
-  ignore (name, create, set, get);
-  []
+  let build n =
+    let store = create () in
+    List.iter (List.init n ~f:Fn.id) ~f:(fun key -> set store ~key ~data:key);
+    store
+  in
+  List.concat_map sizes ~f:(fun n ->
+    let prebuilt = build n in
+    [ Bench.Test.create ~name:(sprintf "%s build (n=%d)" name n) (fun () ->
+        ignore (build n : _))
+    ; Bench.Test.create ~name:(sprintf "%s get_hit (n=%d)" name n) (fun () ->
+        ignore (get prebuilt (present_key n) : int option))
+    ; Bench.Test.create
+        ~name:(sprintf "%s get_miss (n=%d)" name n)
+        (fun () -> ignore (get prebuilt absent_key : int option))
+    ])
 ;;
 
 let bench_sequential =
--- performance/src/sequences.ml
+++ performance/src/sequences.ml
@@ -1,29 +1,51 @@
 open! Core
 
 module List_seq = struct
-  (* TODO: replace the definition of type t and the implementations of
-     create, set, and get *)
-  type t = unit
+  (* A [ref] so that [set] can update in place and return [unit], per the
+     [.mli]. The list stores elements in index order: position [i] in the
+     list is index [i]. Positional [set] is O(n) either way -- that's the
+     point of the comparison. *)
+  type t = int list ref
 
-  let create () = ()
-  let set t ~key ~data = ignore (t, key, data)
+  let create () : t = ref []
 
-  let get t key =
-    ignore (t, key);
-    None
+  (* Three cases, following the [.mli]: out of range raises, [key = length]
+     appends, and [key < length] replaces the element already there. *)
+  let set t ~key ~data =
+    let len = List.length !t in
+    if key < 0 || key > len
+    then
+      raise_s
+        [%message "List_seq.set: index out of range" (key : int) (len : int)]
+    else if key = len
+    then t := !t @ [ data ]
+    else t := List.mapi !t ~f:(fun i x -> if i = key then data else x)
   ;;
+
+  let get t key = List.nth !t key
 end
 
 module Dynarray_seq = struct
-  (* TODO: replace the definition of type t and the implementations of
-     create, set, and get *)
-  type t = unit
+  type t = int Dynarray.t
 
-  let create () = ()
-  let set t ~key ~data = ignore (t, key, data)
+  let create () : t = Dynarray.create ()
+
+  let set t ~key ~data =
+    let len = Dynarray.length t in
+    match key < 0 || key > len with
+    | true ->
+      raise_s
+        [%message
+          "Dynarray_seq.set: index out of range" (key : int) (len : int)]
+    | false ->
+      (match key = len with
+       | false -> Dynarray.set t key data
+       | true -> Dynarray.add_last t data)
+  ;;
 
   let get t key =
-    ignore (t, key);
-    None
+    match key < 0 || key >= Dynarray.length t with
+    | true -> None
+    | false -> Some (Dynarray.get t key)
   ;;
 end
